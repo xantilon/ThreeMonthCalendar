0ab04e134017ef80378e0f87b023b82acbe6bfd7
Edgar Holzke <e.holzke@hamann-software.de>
2023-01-12 19:13:53 +0100

    finetuning

--- ThreeMonthCalendar/Pages/Index.cshtml.cs
+++ ThreeMonthCalendar/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ThreeMonthCalendar.Pages
 {
@@ -9,37 +10,44 @@ namespace ThreeMonthCalendar.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        
+        public DateTime StartDate { get; set; }
+        public List<Employee> Employees { get; set; }
+        public string? CssMonthBorder(int day) { return (day == 1) ? "month-border" : null; }
+        public string? CssWeekend(DayOfWeek wday) { return (DayOfWeek.Sunday == wday || DayOfWeek.Saturday == wday) ? "cal-weekend" : "cal-weekday"; }
+
+
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            StartDate = new DateTime(2022, 12, 10);
         }
 
         public void OnGet()
         {
-            var startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1); ;
-            ViewData["startDate"] = startDate;
-            ViewData["employees"] = new List<Employee>
+            DateTime wannGehtsLos = DateTime.Now.AddMonths(0);
+            Employees = new List<Employee>
             {
                 new Employee("Adrian", new List<Leave>
                 {
-                    new Leave(startDate.AddDays(0), eLeaveType.Urlaub),
-                    new Leave(startDate.AddDays(4), eLeaveType.Urlaub)
+                    new Leave(wannGehtsLos.AddDays(0), eLeaveType.Urlaub),
+                    new Leave(wannGehtsLos.AddDays(4), eLeaveType.Urlaub)
                 }),
                 new Employee("Edgar", new List<Leave>
                 {
-                    new Leave(startDate.AddDays(7), eLeaveType.Urlaub),
-                    new Leave(startDate.AddDays(8), eLeaveType.Urlaub)
+                    new Leave(wannGehtsLos.AddDays(7), eLeaveType.Urlaub),
+                    new Leave(wannGehtsLos.AddDays(8), eLeaveType.Urlaub)
                 }),
                 new Employee("Farshid", new List<Leave>
                 {
-                    new Leave(startDate.AddDays(11), eLeaveType.Urlaub),
-                    new Leave(startDate.AddDays(12), eLeaveType.Sonder)
+                    new Leave(wannGehtsLos.AddDays(11), eLeaveType.Urlaub),
+                    new Leave(wannGehtsLos.AddDays(12), eLeaveType.Sonder)
                 }),
                 new Employee("Marcus", new List<Leave>
                 {
-                    new Leave(startDate.AddDays(30), eLeaveType.Urlaub),
-                    new Leave(startDate.AddDays(18), eLeaveType.Krank)
+                    new Leave(wannGehtsLos.AddDays(30), eLeaveType.Urlaub),
+                    new Leave(wannGehtsLos.AddDays(18), eLeaveType.Krank)
                 })
             };
         }
